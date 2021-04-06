4482745debb40bb5b85359831ce22184e419938c
rauldtrejo <rdanieltrejoguzman@gmail.com>
2021-04-06 14:45:46 -0700

    Added signup and know the capital game mode url

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/signup', views.signup, name='signup'),
+    path('game/capitals/', views.game_capitals, name='game_capitals')
 ]
