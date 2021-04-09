7362656240df30ba81d4c74e9f3f74fb4af7f811
rauldtrejo <rdanieltrejoguzman@gmail.com>
2021-04-08 18:40:55 -0700

    Added capitals_correct, capitals_correct_answer, capitals_incorrect_answer

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup', views.signup, name='signup'),
     path('game/capitals/<int:state_id>/', views.game_capitals, name='game_capitals'),
-    path('game/capitals_incorrect/<int:state_id>', views.game_capitals_incorrect, name='game_capitals_incorrect')
+    path('game/capitals_correct/<int:state_id>', views.game_capitals_correct, name='game_capitals_correct'),
+    path('game/capitals_incorrect/<int:state_id>', views.game_capitals_incorrect, name='game_capitals_incorrect'),
+    path('game/capitals_correct_answer/<int:state_id>', views.game_capitals_correct_answer, name='game_capitals_correct_answer'),
+    path('game/capitals_incorrect_answer/<int:state_id>', views.game_capitals_incorrect_answer, name='game_capitals_incorrect_answer')
 ]
