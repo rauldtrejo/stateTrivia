452a9bcbf36722f1b3a835a69aba26827200727e
rauldtrejo <rdanieltrejoguzman@gmail.com>
2021-04-09 08:38:42 -0700

    Added views for mottos game

--- main_app/views.py
+++ main_app/views.py
@@ -3,19 +3,40 @@ from main_app.models import State
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from .models import Progress, State
+from .models import Progress, State, Score
 from random import shuffle
+
 
 # Create your views here.
 
 def home(request):
-  user_progress, created = Progress.objects.get_or_create(
+  user_progress_capitals, created = Progress.objects.get_or_create(
   user = request.user,
   game_mode = 'capitals',
-  defaults={'current_state': 0, 'incorrect':0}
+  defaults={'correct': 0, 'incorrect':0}
   )
-  user_left_off = user_progress.current_state + user_progress.incorrect + 1
-  return render(request, 'landingPage.html', {'user_left_off': user_left_off})
+  user_left_off_capitals = user_progress_capitals.correct + user_progress_capitals.incorrect + 1
+
+  user_progress_mottos, created = Progress.objects.get_or_create(
+  user = request.user,
+  game_mode = 'mottos',
+  defaults={'correct': 0, 'incorrect':0}
+  )
+  user_left_off_mottos = user_progress_mottos.correct + user_progress_mottos.incorrect + 1
+
+  user_progress_extreme, created = Progress.objects.get_or_create(
+  user = request.user,
+  game_mode = 'extreme',
+  defaults={'correct': 0, 'incorrect':0}
+  )
+  user_left_off_extreme = user_progress_extreme.correct + user_progress_extreme.incorrect + 1
+
+  context={
+    'user_left_off_capitals' : user_left_off_capitals,
+    'user_left_off_mottos' : user_left_off_mottos,
+    'user_left_off_extreme' : user_left_off_extreme,
+  }
+  return render(request, 'landingPage.html', context)
 
 def signup(request):
   error_message = ''
@@ -50,7 +71,6 @@ def game_capitals(request, state_id):
     for state in all_states:
       shuffleArray.append(state)
 
-
     shuffle(shuffleArray)
 
     AnswerArray.append(current_state.capital)
@@ -58,7 +78,6 @@ def game_capitals(request, state_id):
       AnswerArray.append(shuffleArray[x].capital)
 
     shuffle(AnswerArray)
-    print(AnswerArray)
 
     context = {
       'current_state': current_state,
@@ -67,13 +86,47 @@ def game_capitals(request, state_id):
       'progress': progress,
       'user_progress': user_progress
     }
-    return render (request, 'gameModes/capitals.html', context)
+    return render (request, 'gameModes/capitals/capitals.html', context)
   else:
-    user_progress.current_state = 0
+    user_progress.correct = 0
     user_progress.incorrect = 0
     user_progress.save()
     return redirect('home')
 
+def game_mottos(request, state_id):
+  user_progress = Progress.objects.get(user = request.user, game_mode = 'mottos')
+  if(state_id <= 50):
+    all_states = State.objects.all()
+    current_state = State.objects.get(id=state_id)
+    next_state = current_state.id + 1
+    shuffleArray = []
+    AnswerArray = []
+    progress = (current_state.id/50)*100
+
+    for state in all_states:
+      shuffleArray.append(state)
+
+    shuffle(shuffleArray)
+
+    AnswerArray.append(current_state.motto)
+    for x in range(2):
+      AnswerArray.append(shuffleArray[x].motto)
+
+    shuffle(AnswerArray)
+
+    context = {
+      'current_state': current_state,
+      'next_state': next_state,
+      'AnswerArray': AnswerArray,
+      'progress': progress,
+      'user_progress': user_progress
+    }
+    return render (request, 'gameModes/mottos/mottos.html', context)
+  else:
+    user_progress.correct = 0
+    user_progress.incorrect = 0
+    user_progress.save()
+    return redirect('home')
 
 
 def game_capitals_incorrect(request, state_id):
@@ -87,7 +140,7 @@ def game_capitals_incorrect(request, state_id):
     'progress': progress,
     'user_progress': user_progress
   }
-  return render (request, 'gameModes/capitals_incorrect.html', context)
+  return render (request, 'gameModes/capitals/capitals_incorrect.html', context)
 
 
 def game_capitals_correct(request, state_id):
@@ -101,16 +154,43 @@ def game_capitals_correct(request, state_id):
     'progress': progress,
     'user_progress': user_progress
   }
-  return render (request, 'gameModes/capitals_correct.html', context)
+  return render (request, 'gameModes/capitals/capitals_correct.html', context)
+
+def game_mottos_incorrect(request, state_id):
+  user_progress = Progress.objects.get(user = request.user, game_mode = 'mottos')
+  current_state = State.objects.get(id=state_id)
+  next_state = current_state.id + 1
+  progress = (current_state.id/50)*100
+  context = {
+    'current_state': current_state,
+    'next_state': next_state,
+    'progress': progress,
+    'user_progress': user_progress
+  }
+  return render (request, 'gameModes/mottos/mottos_incorrect.html', context)
+
+
+def game_mottos_correct(request, state_id):
+  user_progress = Progress.objects.get(user = request.user, game_mode = 'mottos')
+  current_state = State.objects.get(id=state_id)
+  next_state = current_state.id + 1
+  progress = (current_state.id/50)*100
+  context = {
+    'current_state': current_state,
+    'next_state': next_state,
+    'progress': progress,
+    'user_progress': user_progress
+  }
+  return render (request, 'gameModes/mottos/mottos_correct.html', context)
 
 
 def game_capitals_correct_answer(request, state_id):
   user_progress, created = Progress.objects.get_or_create(
   user = request.user,
   game_mode = 'capitals',
-  defaults={'current_state': 0, 'incorrect':0}
+  defaults={'correct': 0, 'incorrect':0}
   )
-  user_progress.current_state += 1
+  user_progress.correct += 1
   user_progress.save()
 
   return redirect('game_capitals_correct', state_id=state_id)
@@ -119,10 +199,42 @@ def game_capitals_incorrect_answer(request, state_id):
   user_progress, created = Progress.objects.get_or_create(
   user = request.user,
   game_mode = 'capitals',
-  defaults={'current_state': 0, 'incorrect':0}
+  defaults={'correct': 0, 'incorrect':0}
   )
   user_progress.incorrect += 1
   user_progress.save()
 
   return redirect('game_capitals_incorrect', state_id=state_id)
+
+def game_mottos_correct_answer(request, state_id):
+  user_progress, created = Progress.objects.get_or_create(
+  user = request.user,
+  game_mode = 'mottos',
+  defaults={'correct': 0, 'incorrect':0}
+  )
+  current_state = State.objects.get(id=state_id)
+  user_score, created = Score.objects.get_or_create(
+  user = request.user,
+  state = current_state,
+  game_mode = 'mottos',
+  defaults={'correct': 0, 'incorrect': 0, 'total_points': 0}  
+  )
+  user_progress.correct += 1
+  user_score.correct += 1
+  user_score.total_points +=100
+  user_progress.save()
+  user_score.save()
+
+  return redirect('game_mottos_correct', state_id=state_id)
+
+def game_mottos_incorrect_answer(request, state_id):
+  user_progress, created = Progress.objects.get_or_create(
+  user = request.user,
+  game_mode = 'mottos',
+  defaults={'correct': 0, 'incorrect':0}
+  )
+  user_progress.incorrect += 1
+  user_progress.save()
+
+  return redirect('game_mottos_incorrect', state_id=state_id)
 
