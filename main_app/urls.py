aa834b78b35d80ba997ed525b61d55ca37a8753d
rauldtrejo <rdanieltrejoguzman@gmail.com>
2021-04-12 01:42:52 -0700

    Added Game Completed and New Game, and about urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,10 +4,12 @@ from .views import game_logic, profile, navigation, extreme
 urlpatterns = [
     path('', navigation.home, name='home'),
     path('accounts/signup', navigation.signup, name='signup'),
-    path('user/score/<str:game_mode>/', navigation.user_score, name='user_score'),
     path('highscores/', navigation.highscores, name='highscores'),
+    path('about/', navigation.about, name='about'),
 
     path('game/<str:game_mode>/', game_logic.game, name='game'),
+    path('new_game/<str:game_mode>/', game_logic.new_game, name='new_game'),
+    path('game_completed/<str:game_mode>/', game_logic.game_completed, name='game_completed'),
     path('correct_answer/<str:game_mode>/', game_logic.correct_answer, name='correct_answer'),
     path('incorrect_answer/<str:game_mode>/', game_logic.incorrect_answer, name='incorrect_answer'),
     path('correct_answer_page/<str:game_mode>/', game_logic.correct_answer_page, name='correct_answer_page'),
@@ -18,5 +20,5 @@ urlpatterns = [
     path('user/edit_username', profile.edit_username, name='edit_username'),
     path('user/edit_password', profile.edit_password, name='edit_password'),
     path('user/delete_account', profile.delete_account, name='delete_account'),
-
+    path('user/score/<str:game_mode>/', profile.user_score, name='user_score'),
 ]
